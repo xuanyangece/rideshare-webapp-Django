e5d227a3777496beeed65631b3f8ab02b13a33a1
Xuan Yang <xy91@duke.edu>
2019-01-28 22:17:24 -0500

    ride models basically set up

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import timedelta, datetime
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -12,3 +13,12 @@ class UserProfile(models.Model):
     capacity = models.IntegerField(default=1, validators=[MaxValueValidator(200),MinValueValidator(1)])
     special = models.CharField(max_length=200, blank=True)
     
+class Ride(models.Model):
+    status = models.CharField(max_length=10, default='open')
+    destination = models.CharField(max_length=50, blank=False)
+    arrivaldate = models.DateTimeField()
+    passenger = models.IntegerField(validators=[MaxValueValidator(200),MinValueValidator(1)])
+    sharable = models.BooleanField(default=False)
+    vehicle = models.CharField(max_length=20)
+    special = models.CharField(max_length=200)
+    users = models.ManyToManyField(UserProfile)
